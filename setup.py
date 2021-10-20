0ff9ca33acccdacd4613d10f4713916026149778
vodiylik <shookrullo@gmail.com>
2021-10-20 16:19:13 +0500

    some changes

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name='allauth-idegovuz',
+    name='idegovuz',
+    packages=['idegovuz'],
     version='0.1',
+    license='MIT',
     author="Shukrullo Turgunov",
     author_email="sh@turgunov.uz",
     description="id.egov.uz provider for django-allauth",
+    keywords=['id.egov.uz', 'id', 'allauth', 'provider'],
     url="https://github.com/vodiylik/allauth-idegovuz.git",
     packages=setuptools.find_packages(),
     install_requires=['django', 'django-allauth'],
